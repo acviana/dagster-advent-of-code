789bc42b5b45e91b7e7d10f9c94958bb154b4606
Alex C. Viana <alexcostaviana@gmail.com>
2021-03-11 22:15:01 -0600

    First working unparameterized pipeline

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ The solutions are ported from my original implementations in this [repo](https:/
 Note, to install grpcio with Poetry 1.1.4 and Python 3.8.6 on OSX 11.2.2 you need to set the following flag `export GRPC_PYTHON_BUILD_SYSTEM_ZLIB=true` ([source](https://github.com/grpc/grpc/issues/24677#issuecomment-728721822)).
 
 - 0.0.1: Project setup and working pure Python implementation of Day 1 solution. No Dagster elements
+- 0.0.2: Working non-parameterized Dagster pipeline with basic tye hints
--- dagster_advent_of_code/day_1_pipeline.py
+++ dagster_advent_of_code/day_1_pipeline.py
@@ -2,36 +2,51 @@ from itertools import combinations
 from math import prod
 from typing import List, Tuple
 
-
-def load_data() -> List[int]:
-    with open("inputs/day_1_input.txt", "r") as f:
-        return [int(item.strip()) for item in f.readlines()]
+from dagster import execute_pipeline, pipeline, solid
 
 
-def match_by_sum_and_multiply(
-    input_list: List[int], set_size: int, match_sum: int
-) -> Tuple:
+@solid
+def load_data(context) -> List[int]:
+    file_path = "data/day_1_input.txt"
+    context.log.info(f"Attempting to load data from {file_path}")
+    with open(file_path, "r") as f:
+        dataset = [int(item.strip()) for item in f.readlines()]
+    context.log.info(f"Loaded dataset with {len(dataset)} elements")
+    return dataset
+
+
+@solid
+def match_by_sum(context, input_list: List[int]) -> Tuple:
+    # , set_size: int, match_sum: int
+    set_size = 2
+    match_sum = 2020
     for combination in combinations(input_list, set_size):
         if sum(combination) == match_sum:
+            context.log.info(f"Found combination {combination} with sum {match_sum}")
             return combination
+    context.log.info(f"Found no combinations with sum {match_sum}")
     return ()
 
 
-def summerize_result(combination: Tuple) -> str:
-    return (
+@solid
+def summerize_result(context, combination: Tuple) -> str:
+    result_summery = (
         f"Set: {*combination,}, Sum: {sum(combination)}, Product: {prod(combination)}"
     )
+    context.log.info(result_summery)
+    return result_summery
 
 
-def main() -> None:
+@pipeline
+def day_1_pipeline():
     data = load_data()
+    combination = match_by_sum(data)  # , 2, 2020)
+    result_summery = summerize_result(combination)
+    # print(summerize_result(combination))
 
-    combination = match_by_sum_and_multiply(data, 2, 2020)
-    print(summerize_result(combination))
-
-    combination = match_by_sum_and_multiply(data, 3, 2020)
-    print(summerize_result(combination))
+    # combination = match_by_sum_and_multiply(data, 3, 2020)
+    # print(summerize_result(combination))
 
 
 if __name__ == "__main__":
-    main()
+    result = execute_pipeline(day_1_pipeline)

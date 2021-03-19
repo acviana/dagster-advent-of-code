9c2c5f4c63e26da7a832a8044a4a28a2df9292b4
Alex C. Viana <alexcostaviana@gmail.com>
2021-03-19 12:23:14 -0500

    Working parameterized pipeline

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ The solutions are ported from my original implementations in this [repo](https:/
 Note, to install grpcio with Poetry 1.1.4 and Python 3.8.6 on OSX 11.2.2 you need to set the following flag `export GRPC_PYTHON_BUILD_SYSTEM_ZLIB=true` ([source](https://github.com/grpc/grpc/issues/24677#issuecomment-728721822)).
 
 - 0.0.1: Project setup and working pure Python implementation of Day 1 solution. No Dagster elements
-- 0.0.2: Working non-parameterized Dagster pipeline with basic tye hints
+- 0.0.2: Working non-parameterized Dagster pipeline with basic type hints
+- 0.0.2: Parameterized Dagster pipeline with YAML configuration files
--- dagster_advent_of_code/day_1_pipeline.py
+++ dagster_advent_of_code/day_1_pipeline.py
@@ -2,12 +2,11 @@ from itertools import combinations
 from math import prod
 from typing import List, Tuple
 
-from dagster import execute_pipeline, pipeline, solid
+from dagster import pipeline, solid  # type: ignore
 
 
 @solid
-def load_data(context) -> List[int]:
-    file_path = "data/day_1_input.txt"
+def load_data(context, file_path: str) -> List[int]:
     context.log.info(f"Attempting to load data from {file_path}")
     with open(file_path, "r") as f:
         dataset = [int(item.strip()) for item in f.readlines()]
@@ -16,15 +15,16 @@ def load_data(context) -> List[int]:
 
 
 @solid
-def match_by_sum(context, input_list: List[int]) -> Tuple:
-    # , set_size: int, match_sum: int
-    set_size = 2
-    match_sum = 2020
+def match_by_sum(
+    context, input_list: List[int], set_size: int, match_sum: int
+) -> Tuple:
     for combination in combinations(input_list, set_size):
         if sum(combination) == match_sum:
-            context.log.info(f"Found combination {combination} with sum {match_sum}")
+            context.log.info(
+                f"Found {set_size} item combination {combination} with sum {match_sum}"
+            )
             return combination
-    context.log.info(f"Found no combinations with sum {match_sum}")
+    context.log.info(f"Found no {set_size} item combinations with sum {match_sum}")
     return ()
 
 
@@ -42,11 +42,3 @@ def day_1_pipeline():
     data = load_data()
     combination = match_by_sum(data)  # , 2, 2020)
     result_summery = summerize_result(combination)
-    # print(summerize_result(combination))
-
-    # combination = match_by_sum_and_multiply(data, 3, 2020)
-    # print(summerize_result(combination))
-
-
-if __name__ == "__main__":
-    result = execute_pipeline(day_1_pipeline)
